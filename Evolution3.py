3d058f64496909a56679e4a67b4555be818bf5a4
PiaoXueGZ <759997834@qq.com>
2023-08-09 18:02:45 +0800

    Bug fixes and better packaging

--- Evolution3.py
+++ Evolution3.py
@@ -2,7 +2,7 @@ import numpy as np
 import geatpy as ea
 
 class MyProblem(ea.Problem):  # 继承Problem父类
-    def __init__(self, name, M, maxormins, Dim, varTypes, lb, ub, lbin, ubin):  # M 目标变量维数f的个数
+    def __init__(self, name, M, maxormins, Dim, varTypes, lb, ub, lbin, ubin, f):
         self.name = name  # 初始化name（函数名称，可以随意设置）'MyProblem'
         self.M = M #目标函数维数
         self.maxormins = maxormins  # 初始化maxormins（目标最小最大化标记列表，1：最小化该目标；-1：最大化该目标）[1]
@@ -13,97 +13,42 @@ class MyProblem(ea.Problem):  # 继承Problem父类
         self.ub = ub * Dim  # 决策变量上界 [9]
         self.lbin = lbin * Dim  # 决策变量下边界（0表示不包含该变量的下边界，1表示包含）[1]
         self.ubin = ubin * Dim  # 决策变量上边界（0表示不包含该变量的上边界，1表示包含）[1]
+        self.f = f  # 目标函数
+        
         # 调用父类构造方法完成实例化
         ea.Problem.__init__(self, self.name, self.M, self.maxormins, self.Dim, self.varTypes, self.lb, self.ub, self.lbin, self.ubin)
 
     def evalVars(self, Vars):  # 目标函数
-        x0 = Vars[:, [0]]
-        x1 = Vars[:, [1]]
-        x2 = Vars[:, [2]]
-        x3 = Vars[:, [3]]
-        x4 = Vars[:, [4]]
-        x5 = Vars[:, [5]]
-        x6 = Vars[:, [6]]
-        x7 = Vars[:, [7]]
-        x8 = Vars[:, [8]]
-        x9 = Vars[:, [9]]
-        f = (x0 - 0)**2 +(x1 - 1) ** 2 +(x2 - 2) ** 2 +(x3 - 3) ** 2 +(x4 - 4)**2 +(x5 - 5) ** 2 +(x6 - 6) ** 2 +(x7 - 7) ** 2 +(x8 - 8) ** 2 +(x9 - 9) ** 2
+        fitnesses = []
+        for var in Vars:
+            fitnesses.append(self.f(var))
 
-        return f
+        return np.array([fitnesses]).T
 
-    def Evolution(self,
-                 Problem=None,
-                 XOVR=None,
-                 Pm=None,
-                 Encoding='RI',
-                 NIND=None,
-                 MAXGEN=None,
-                 MAXTIME=None,
-                 MAXEVALS=None,
-                 MAXSIZE=None,
-                 logTras=None,
-                 prophet=None,
-                 seed=None,
-                 verbose=None,
-                 outFunc=None,
-                 drawing=None,
-                 outputMsg=None,
-                 drawLog=None,
-                 saveFlag=None,
-                 trappedValue=None,
-                 maxTrappedCount=None,
-                 dirName=None,
-                 **kwargs):
-        #构建算法
-        algorithm = ea.soea_SGA_templet(
-            problem,
-            ea.Population(Encoding=Encoding, Field=None, NIND=NIND, Chrom=None, ObjV=None, FitnV=None, CV=None, Phen=None),
-            MAXGEN,
-            MAXTIME,
-            MAXEVALS,
-            MAXSIZE,
-            logTras,
-            verbose,
-            outFunc,
-            drawing,
-            trappedValue,
-            maxTrappedCount,
-            dirName,
-            **kwargs
-            )
+class EvolutionAlgorithm:
+    def __init__(self, Dim, varTypes, lb, ub, lbin, ubin, f, M = 1, maxormins = [1], name = "myProblem"):
+        '''
+        传入进化算法参数：
+        name  # 函数名称，可以随意设置
+        M #目标函数维数
+        maxormins  # 初始化maxormins（目标最小最大化标记列表，1：最小化该目标；-1：最大化该目标）
+        # 离散变量则添加此行 self.var_set = np.array([1.1, 1, 0, 3, 5.5, 7.2,9])  # 设定一个集合，要求决策变量的值取自于该集合
+        Dim  # 初始化Dim（决策变量维数）
+        varTypes = varTypes  # 初始化varTypes（决策变量的类型，元素为0表示对应的变量是连续的；1表示是离散的）
+        lb  # 决策变量下界
+        ub # 决策变量上界
+        lbin # 决策变量下边界（0表示不包含该变量的下边界，1表示包含）
+        ubin # 决策变量上边界（0表示不包含该变量的上边界，1表示包含）
+        f  # 目标函数
+        '''
 
-        algorithm.recOper.XOVR=XOVR   # 交叉概率
-        algorithm.mutOper.Pm=Pm       # 变异概率
-        #求解
-        res = ea.optimize(algorithm,
-             seed,
-             prophet,
-             verbose,
-             drawing,
-             outputMsg,
-             drawLog,
-             saveFlag,
-             dirName,
-             **kwargs)
-        print(res)
-
-
-if __name__ == '__main__':   #主函数
-    # 实例化问题对象
-    problem = MyProblem(
-        name='MyProblem',  # 初始化name（函数名称，可以随意设置）'MyProblem'
-        M=1,               # M (目标函数维数)
-        maxormins=[1],     # maxormins（目标最小最大化标记列表，1：最小化该目标；-1：最大化该目标）
-        Dim=10,            # Dim（决策变量维数）
-        varTypes=[0],      # varTypes（决策变量的类型，元素为0表示对应的变量是连续的；1表示是离散的）
-        lb=[0],            # lb 决策变量下界
-        ub=[9],            # ub 决策变量上界
-        lbin=[1],          # lbin 决策变量下边界（0表示不包含该变量的下边界，1表示包含）
-        ubin=[1]           # ubin 决策变量上边界（0表示不包含该变量的下边界，1表示包含）
-        )
-    # 执行进化算法
-    MyProblem.Evolution(
-        problem,
+        self.problem = MyProblem(name, M, maxormins, Dim, varTypes, lb, ub, lbin, ubin, f)
+        
+    def Evolution(self, XOVR = 0.7, Pm=0.05, Encoding='RI', NIND=50, MAXGEN=1000, MAXTIME=None, MAXEVALS=None,
+                 MAXSIZE=None, logTras=0, prophet=None, seed=None, verbose=False, outFunc=None, drawing=0, outputMsg=False,
+                 drawLog=False, saveFlag=True, trappedValue=None, maxTrappedCount=None, dirName=None, **kwargs):
+        '''
+        对给定的优化函数进行进化计算
         XOVR = 0.7,          # 交叉概率
         Pm = 0.05,           # 变异概率
         Encoding = 'RI',     # str - 染色体编码方式，'BG':二进制/格雷编码；'RI':实整数编码，即实数和整数的混合编码；'P':排列编码。
@@ -126,6 +71,13 @@ if __name__ == '__main__':   #主函数
         trappedValue = None,
         maxTrappedCount = None,
         dirName = None,      # str - 文件保存的路径。当缺省或为None时，默认保存在当前工作目录的'result of job xxxx-xx-xx xxh-xxm-xxs'文件夹下。
-        )
+        '''
 
+        algorithm = ea.soea_SGA_templet(self.problem, ea.Population(Encoding=Encoding, Field=None, NIND=NIND, Chrom=None, ObjV=None, FitnV=None, CV=None, Phen=None),
+            MAXGEN, MAXTIME, MAXEVALS, MAXSIZE, logTras, verbose, outFunc, drawing, trappedValue, maxTrappedCount, dirName, **kwargs)
+        
+        algorithm.recOper.XOVR = XOVR
+        algorithm.mutOper.Pm = Pm
 
+        res = ea.optimize(algorithm, seed, prophet, verbose, drawing, outputMsg, drawLog, saveFlag, dirName, **kwargs)
+        return res
